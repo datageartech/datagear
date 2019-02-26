1736020d0bce4a553f51decfb3e0e8e7d2d8a18b
datagear <datagear@163.com>
2019-02-26 10:52:00 +0800

    解决集成软件包无法在JDK1.6环境运行的问题；

--- datagear-webembd/src/main/dist/startup.sh
+++ datagear-webembd/src/main/dist/startup.sh
@@ -3,6 +3,8 @@
 JAVA_HOME=$JAVA_HOME
 JAVA_OPTS=$JAVA_OPTS
 
+APP_CLASSPATH=.:lib/datagear-webembd-1.2.0.jar:lib/hamcrest-core-1.3.jar:lib/javax.servlet-3.0.0.v201112011016.jar:lib/jetty-all-8.1.22.v20160922.jar:lib/junit-4.11.jar::lib/log4j-1.2.17.jar:lib/slf4j-api-1.7.9.jar:lib/slf4j-log4j12-1.7.9.jar
+
 APP_NAME="Data Gear"
 ECHO_PREFIX="[$APP_NAME]  :"
 BORDER="========================================="
@@ -31,7 +33,7 @@ if [ $APP_PID -ne 0 ]; then
 	echo "$ECHO_PREFIX starting [Failed]"
 else
 	echo "$ECHO_PREFIX starting..."
-	nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp .:lib/* $APP_MAIN >/dev/null 2>&1 &
+	nohup $JAVA_HOME/bin/java $JAVA_OPTS -cp $APP_CLASSPATH $APP_MAIN >/dev/null 2>&1 &
 	readAppPID
 	if [ $APP_PID -ne 0 ]; then
 		echo "$ECHO_PREFIX PID is $APP_PID"
