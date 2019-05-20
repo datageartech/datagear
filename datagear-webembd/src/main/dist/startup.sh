cb925588803bbdeebbb719f70a594a8ad22e18bc
datagear <datagear@163.com>
2019-05-20 09:34:46 +0800

    修改startup脚本中库版本错误导致应用无法启动的BUG

--- datagear-webembd/src/main/dist/startup.sh
+++ datagear-webembd/src/main/dist/startup.sh
@@ -3,7 +3,7 @@
 JAVA_HOME=$JAVA_HOME
 JAVA_OPTS=$JAVA_OPTS
 
-APP_CLASSPATH=.:lib/datagear-webembd-1.2.0.jar:lib/hamcrest-core-1.3.jar:lib/javax.servlet-3.0.0.v201112011016.jar:lib/jetty-all-8.1.22.v20160922.jar:lib/junit-4.11.jar::lib/log4j-1.2.17.jar:lib/slf4j-api-1.7.9.jar:lib/slf4j-log4j12-1.7.9.jar
+APP_CLASSPATH=.:lib/datagear-webembd-1.3.0.jar:lib/hamcrest-core-1.3.jar:lib/javax.servlet-3.0.0.v201112011016.jar:lib/jetty-all-8.1.22.v20160922.jar:lib/junit-4.11.jar::lib/log4j-1.2.17.jar:lib/slf4j-api-1.7.9.jar:lib/slf4j-log4j12-1.7.9.jar
 
 APP_NAME="Data Gear"
 ECHO_PREFIX="[$APP_NAME]  :"
