7269cb85feacabbaea245643ea6e3c09302cc26f
datagear <datagear@163.com>
2019-10-28 12:26:56 +0800

    完善JSON导出功能

--- datagear-dataexchange/src/main/java/org/datagear/dataexchange/support/JsonDataExportService.java
+++ datagear-dataexchange/src/main/java/org/datagear/dataexchange/support/JsonDataExportService.java
@@ -5,8 +5,13 @@
 package org.datagear.dataexchange.support;
 
 import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +23,7 @@ import javax.json.stream.JsonGeneratorFactory;
 import org.datagear.dataexchange.AbstractDevotedDbInfoAwareDataExchangeService;
 import org.datagear.dataexchange.DataExchangeContext;
 import org.datagear.dataexchange.DataExchangeException;
+import org.datagear.dataexchange.DataFormatContext;
 import org.datagear.dataexchange.IndexFormatDataExchangeContext;
 import org.datagear.dataexchange.RowDataIndex;
 import org.datagear.dataexchange.TextDataExportListener;
@@ -86,6 +92,7 @@ public class JsonDataExportService extends AbstractDevotedDbInfoAwareDataExchang
 	 * @param out
 	 * @param exportContext
 	 */
+	@SuppressWarnings("resource")
 	protected void writeRecords(JsonDataExport dataExchange, Connection cn, List<ColumnInfo> columnInfos, ResultSet rs,
 			Writer out, IndexFormatDataExchangeContext exportContext) throws Throwable
 	{
@@ -95,7 +102,6 @@ public class JsonDataExportService extends AbstractDevotedDbInfoAwareDataExchang
 
 		int columnCount = columnInfos.size();
 
-		@SuppressWarnings("resource")
 		JsonGenerator generator = (exportOption.isPrettyPrint() ? FACTORY_PRETTY_PRINT.createGenerator(out)
 				: FACTORY.createGenerator(out));
 
@@ -124,30 +130,8 @@ public class JsonDataExportService extends AbstractDevotedDbInfoAwareDataExchang
 			{
 				ColumnInfo columnInfo = columnInfos.get(i);
 
-				String value = null;
-
-				try
-				{
-					value = getStringValue(cn, rs, i + 1, columnInfo.getType(), exportContext.getDataFormatContext());
-				}
-				catch (Throwable t)
-				{
-					if (exportOption.isNullForIllegalColumnValue())
-					{
-						value = null;
-
-						if (listener != null)
-							listener.onSetNullTextValue(exportContext.getDataIndex(), columnInfo.getName(),
-									wrapToDataExchangeException(t));
-					}
-					else
-						throw t;
-				}
-
-				if (value == null)
-					generator.writeNull(columnInfo.getName());
-				else
-					generator.write(columnInfo.getName(), value);
+				writeJsonValue(dataExchange, cn, columnInfos, rs, exportContext, generator, i + 1, columnInfo,
+						exportOption, jsonDataFormat, listener);
 			}
 
 			generator.writeEnd();
@@ -169,5 +153,111 @@ public class JsonDataExportService extends AbstractDevotedDbInfoAwareDataExchang
 		}
 
 		generator.flush();
+	}
+
+	/**
+	 * 写JSON值。
+	 * 
+	 * @param dataExchange
+	 * @param cn
+	 * @param columnInfos
+	 * @param rs
+	 * @param exportContext
+	 * @param generator
+	 * @param column
+	 * @param columnInfo
+	 * @param exportOption
+	 * @param jsonDataFormat
+	 * @param listener
+	 * @throws Throwable
+	 */
+	protected void writeJsonValue(JsonDataExport dataExchange, Connection cn, List<ColumnInfo> columnInfos,
+			ResultSet rs, IndexFormatDataExchangeContext exportContext, JsonGenerator generator, int column,
+			ColumnInfo columnInfo, JsonDataExportOption exportOption, JsonDataFormat jsonDataFormat,
+			TextDataExportListener listener) throws Throwable
+	{
+		String name = columnInfo.getName();
+		Object value = null;
+
+		try
+		{
+			value = getValue(cn, rs, column, columnInfo.getType());
+
+			if (value == null)
+			{
+				generator.writeNull(name);
+			}
+			else if (value instanceof Number)
+			{
+				Number number = (Number) value;
+
+				if (number instanceof Integer)
+					generator.write(name, number.intValue());
+				else if (number instanceof Long)
+					generator.write(name, number.longValue());
+				else if (number instanceof Float || number instanceof Double)
+					generator.write(name, number.doubleValue());
+				else if (number instanceof BigDecimal)
+					generator.write(name, (BigDecimal) number);
+				else if (number instanceof BigInteger)
+					generator.write(name, (BigInteger) number);
+				else
+					generator.write(name, number.intValue());
+			}
+			else if (value instanceof Date)
+			{
+				DataFormatContext dataFormatContext = exportContext.getDataFormatContext();
+				String valueStr = null;
+
+				if (value instanceof java.sql.Date)
+					valueStr = dataFormatContext.formatDate((java.sql.Date) value);
+				else if (value instanceof java.sql.Time)
+					valueStr = dataFormatContext.formatTime((Time) value);
+				else if (value instanceof java.sql.Timestamp)
+					valueStr = dataFormatContext.formatTimestamp((Timestamp) value);
+				else
+					valueStr = dataFormatContext.formatDate((java.sql.Date) value);
+
+				if (valueStr == null)
+					generator.writeNull(name);
+				else
+					generator.write(name, valueStr);
+			}
+			else if (value instanceof String)
+			{
+				generator.write(name, (String) value);
+			}
+			else if (value instanceof Boolean)
+			{
+				generator.write(name, (Boolean) value);
+			}
+			else if (value instanceof byte[])
+			{
+				String valueStr = exportContext.getDataFormatContext().formatBytes((byte[]) value);
+
+				if (valueStr == null)
+					generator.writeNull(name);
+				else
+					generator.write(name, valueStr);
+			}
+			else
+			{
+				generator.write(name, value.toString());
+			}
+
+		}
+		catch (Throwable t)
+		{
+			if (exportOption.isNullForIllegalColumnValue())
+			{
+				generator.writeNull(name);
+
+				if (listener != null)
+					listener.onSetNullTextValue(exportContext.getDataIndex(), columnInfo.getName(),
+							wrapToDataExchangeException(t));
+			}
+			else
+				throw t;
+		}
 	}
 }
