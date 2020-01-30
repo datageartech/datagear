133989f44833b5e37bf1273f4835c8b60f88b104
datagear <datagear@163.com>
2020-01-30 12:54:56 +0800

    [web]将web静态资源、视图文件都移至src/main/resources/org/datagear/web/webapp目录内，便于后续整合应用

---
+++ datagear-web/src/main/java/org/datagear/web/controller/StaticResourceController.java
@@ -0,0 +1,45 @@
+package org.datagear.web.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+/**
+ * 静态资源控制器。
+ * <p>
+ * 此控制器负责加载{@code org/datagear/web/webapp/staticResource}类路径内的资源。
+ * </p>
+ * 
+ * @author datagear@163.com
+ *
+ */
+@Controller("/static")
+public class StaticResourceController extends ResourceHttpRequestHandler
+{
+	public static final String STATIC_RESOURCE_CLASSPATH_ROOT = "org/datagear/web/webapp/staticResource/";
+
+	public StaticResourceController()
+	{
+		super();
+
+		List<Resource> locations = new ArrayList<Resource>(1);
+		locations.add(new ClassPathResource(STATIC_RESOURCE_CLASSPATH_ROOT));
+		setLocations(locations);
+	}
+
+	@RequestMapping("/**/*")
+	public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		handleRequest(request, response);
+	}
+}
