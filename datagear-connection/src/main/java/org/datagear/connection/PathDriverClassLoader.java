a9b02941bbe47f034557d20ac86fc8cb01d70698
datagear <datagear@163.com>
2020-03-10 22:33:19 +0800

    [connection]完善数据库驱动类加载器，避免驱动本身的资源、类代理至父类加载器加载

--- datagear-connection/src/main/java/org/datagear/connection/PathDriverClassLoader.java
+++ datagear-connection/src/main/java/org/datagear/connection/PathDriverClassLoader.java
@@ -26,7 +26,10 @@ import org.slf4j.LoggerFactory;
 /**
  * 指定路径的驱动类加载器。
  * <p>
- * 注意：此类会优先从指定路径中加载类，因此，不应该将标准库放入此路径中。
+ * 注意：对于非JDK自带的类，此类加载器仅会从{@linkplain #getPath()}加载，而不会代理给父类加载器。
+ * </p>
+ * <p>
+ * 此规则可以避免驱动类依赖的某些库在{@linkplain #getPath()}中找不到时代理至应用类加载器，而可能出现版本不一致的情况。
  * </p>
  * 
  * @author datagear@163.com
@@ -83,23 +86,21 @@ public class PathDriverClassLoader extends URLClassLoader
 	@Override
 	public URL getResource(String name)
 	{
-		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("start getting resource URL for [" + name + "]");
-
 		URL url = findResource(name);
 
-		if (url == null)
-		{
-			ClassLoader parent = getParentClassLoader();
-
-			if (LOGGER.isDebugEnabled())
-				LOGGER.debug("delegate parent class loader for getting resource URL for [" + name + "]");
-
-			return parent.getResource(name);
-		}
+		// 对于独立的驱动程序类加载器，不应该代理至父类加载器
+//		if (url == null)
+//		{
+//			ClassLoader parent = getParentClassLoader();
+//
+//			if (LOGGER.isDebugEnabled())
+//				LOGGER.debug("delegate parent class loader for getting resource URL for [" + name + "]");
+//
+//			return parent.getResource(name);
+//		}
 
 		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("got resource URL [" + url + "] for [" + name + "] in path [" + getPath() + "]");
+			LOGGER.debug("getResource [" + url + "] for [" + name + "] in path [" + getPath() + "]");
 
 		return url;
 	}
@@ -107,23 +108,21 @@ public class PathDriverClassLoader extends URLClassLoader
 	@Override
 	public Enumeration<URL> getResources(String name) throws IOException
 	{
-		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("start getting resource URLs for [" + name + "]");
-
 		Enumeration<URL> urls = findResources(name);
 
-		if (urls == null || urls.hasMoreElements())
-		{
-			ClassLoader parent = getParentClassLoader();
-
-			if (LOGGER.isDebugEnabled())
-				LOGGER.debug("delegate parent class loader for getting resource URLs for [" + name + "]");
-
-			return parent.getResources(name);
-		}
+		// 对于独立的驱动程序类加载器，不应该代理至父类加载器
+//		if (urls == null || urls.hasMoreElements())
+//		{
+//			ClassLoader parent = getParentClassLoader();
+//
+//			if (LOGGER.isDebugEnabled())
+//				LOGGER.debug("delegate parent class loader for getting resource URLs for [" + name + "]");
+//
+//			return parent.getResources(name);
+//		}
 
 		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("got resource URLs for [" + name + "] in path [" + getPath() + "]");
+			LOGGER.debug("getResources for [" + name + "] in path [" + getPath() + "]");
 
 		return urls;
 	}
@@ -131,37 +130,45 @@ public class PathDriverClassLoader extends URLClassLoader
 	@Override
 	public InputStream getResourceAsStream(String name)
 	{
-		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("start getting resource as stream for [" + name + "]");
-
 		URL url = findResource(name);
 
+		// 对于独立的驱动程序类加载器，不应该代理至父类加载器
+//		if (url == null)
+//		{
+//			ClassLoader parent = getParentClassLoader();
+//
+//			if (LOGGER.isDebugEnabled())
+//				LOGGER.debug("delegate parent class loader for getting resource as stream for [" + name + "]");
+//
+//			return parent.getResourceAsStream(name);
+//		}
+
 		if (url == null)
-		{
-			ClassLoader parent = getParentClassLoader();
+			return null;
 
-			if (LOGGER.isDebugEnabled())
-				LOGGER.debug("delegate parent class loader for getting resource as stream for [" + name + "]");
-
-			return parent.getResourceAsStream(name);
-		}
+		InputStream in = null;
 
 		try
 		{
-			return url.openStream();
+			in = url.openStream();
 		}
 		catch (IOException e)
 		{
-			return null;
 		}
+
+		if (LOGGER.isDebugEnabled())
+			LOGGER.debug("getResourceAsStream [" + in + "] for [" + name + "] in path [" + getPath() + "]");
+
+		return in;
 	}
 
 	@Override
 	protected synchronized Class<?> loadClass(String name, boolean resolve)
 			throws ClassNotFoundException, ClassFormatError
 	{
-		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("start loading class [" + name + "]");
+		// JDK标准库应由父类加载
+		if (isJDKStandardClassName(name))
+			return Class.forName(name, resolve, getParentClassLoader());
 
 		// -拷贝自java.net.URLClassLoader.FactoryURLClassLoader
 		SecurityManager sm = System.getSecurityManager();
@@ -179,57 +186,48 @@ public class PathDriverClassLoader extends URLClassLoader
 
 		if (clazz == null)
 		{
-			// 标准库不应在此加载
-			if (name.startsWith("java"))
-				;
-			else
+			try
 			{
-				try
-				{
-					clazz = findClass(name);
-				}
-				catch (ClassNotFoundException e)
-				{
-					// 找不到类，则代理给父类加载器
-				}
-				catch (ClassFormatError e)
-				{
-					// 类加载出错（比如版本不兼容），则抛出
-					throw e;
-				}
+				clazz = findClass(name);
+			}
+			catch(ClassNotFoundException e)
+			{
+			}
+			catch(ClassFormatError e)
+			{
+				// 类加载出错（比如版本不兼容），则抛出
+				throw e;
 			}
 		}
 
-		if (clazz == null)
+		// 强制在此加载的类
+		if (clazz == null && this.outsideForceLoads != null && this.outsideForceLoads.contains(name))
 		{
-			// 强制在此加载的类
-			if (this.outsideForceLoads != null && this.outsideForceLoads.contains(name))
+			InputStream in = getParentClassLoader().getResourceAsStream(classNameToPath(name));
+
+			if (in == null)
+				throw new ClassNotFoundException(name);
+
+			byte[] bytes = null;
+
+			try
 			{
-				InputStream in = getResourceAsStream(classNameToPath(name));
-
-				if (in == null)
-					throw new ClassNotFoundException(name);
-
-				byte[] bytes = null;
-
-				try
-				{
-					bytes = IOUtil.getBytes(in);
-				}
-				catch (IOException e)
-				{
-					throw new ClassNotFoundException(name, e);
-				}
-				finally
-				{
-					IOUtil.close(in);
-				}
-
-				clazz = defineClass(name, bytes, 0, bytes.length);
+				bytes = IOUtil.getBytes(in);
 			}
+			catch(IOException e)
+			{
+				throw new ClassNotFoundException(name, e);
+			}
+			finally
+			{
+				IOUtil.close(in);
+			}
+
+			clazz = defineClass(name, bytes, 0, bytes.length);
 		}
 
-		if (clazz == null)
+		// 此时仅代理父类加载JDK扩展库，其他都应由此类加载器加载
+		if (clazz == null && isJDKExtClassName(name))
 		{
 			ClassLoader parent = getParentClassLoader();
 
@@ -242,7 +240,34 @@ public class PathDriverClassLoader extends URLClassLoader
 		if (resolve)
 			resolveClass(clazz);
 
+		if (LOGGER.isDebugEnabled())
+			LOGGER.debug("load class [" + (clazz == null ? "null" : clazz.getName()) + "] for name [" + name
+					+ "] in path [" + getPath() + "]");
+
 		return clazz;
+	}
+
+	/**
+	 * 是否JDK标准类名。
+	 * 
+	 * @param name
+	 * @return
+	 */
+	protected boolean isJDKStandardClassName(String name)
+	{
+		return (name.startsWith("java.") || name.startsWith("javax."));
+	}
+
+	/**
+	 * 是否JDK扩展类名。
+	 * 
+	 * @param name
+	 * @return
+	 */
+	protected boolean isJDKExtClassName(String name)
+	{
+		return (name.startsWith("sun.") || name.startsWith("org.ietf.") || name.startsWith("org.omg.")
+				|| name.startsWith("org.w3c.") || name.startsWith("org.xml."));
 	}
 
 	/**
