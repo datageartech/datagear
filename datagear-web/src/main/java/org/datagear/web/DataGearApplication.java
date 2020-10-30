0e5df40e7f13cdc8ed7573dffb14822f48d11400
datagear <datagear@163.com>
2020-10-30 13:58:42 +0800

    springboot迁移

--- datagear-web/src/main/java/org/datagear/web/DataGearApplication.java
+++ datagear-web/src/main/java/org/datagear/web/DataGearApplication.java
@@ -2,8 +2,10 @@ package org.datagear.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.datagear.web.config")
 public class DataGearApplication
 {
 	public static void main(String[] args)
