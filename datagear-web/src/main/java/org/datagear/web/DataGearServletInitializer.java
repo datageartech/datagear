be7ee14abb33e2db69c4cf4074db9952f62fed60
datagear <datagear@163.com>
2020-11-08 12:17:42 +0800

    springboot迁移：添加Servlet容器运行模式时的Banner

--- datagear-web/src/main/java/org/datagear/web/DataGearServletInitializer.java
+++ datagear-web/src/main/java/org/datagear/web/DataGearServletInitializer.java
@@ -4,6 +4,7 @@
 
 package org.datagear.web;
 
+import org.datagear.web.DataGearApplication.DataGearBanner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -18,6 +19,6 @@ public class DataGearServletInitializer extends SpringBootServletInitializer
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
 	{
-		return application.sources(DataGearApplication.class);
+		return application.sources(DataGearApplication.class).banner(new DataGearBanner());
 	}
 }
