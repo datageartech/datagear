d990c4152a486d35d8472e36e7edcb17cc31bb45
datagear <datagear@163.com>
2020-11-18 19:54:00 +0800

    springboot迁移：数据源改为可在application.properties中配置

--- datagear-web/src/main/java/org/datagear/web/config/DataSourceConfig.java
+++ datagear-web/src/main/java/org/datagear/web/config/DataSourceConfig.java
@@ -11,6 +11,7 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -29,7 +30,6 @@ public class DataSourceConfig
 	public DataSourceConfig(Environment environment)
 	{
 		super();
-		this.environment = environment;
 	}
 
 	public Environment getEnvironment()
@@ -43,22 +43,10 @@ public class DataSourceConfig
 	}
 
 	@Bean
+	@ConfigurationProperties(prefix = "datasource")
 	public DataSource dataSource()
 	{
-		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setDriverClassName(org.apache.derby.jdbc.EmbeddedDriver.class.getName());
-		basicDataSource.setUrl(buildDerbyURL());
-
-		return basicDataSource;
-	}
-
-	protected String buildDerbyURL()
-	{
-		return "jdbc:derby:" + getDerbyAbsolutePath() + ";create=true";
-	}
-
-	protected String getDerbyAbsolutePath()
-	{
-		return this.environment.getProperty("directory.derby");
+		// connection模块使用了DPCP2，所以这里也统一采用，避免多引入其他连接池库
+		return new BasicDataSource();
 	}
 }
