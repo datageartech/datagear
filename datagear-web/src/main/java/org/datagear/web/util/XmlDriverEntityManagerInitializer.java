5555f694dffa69a8b22001df911ff08abca14c35
datagear <datagear@163.com>
2021-06-02 15:00:49 +0800

    修复系统自动升级时，未自动升级内置驱动库缺失库文件的BUG

--- datagear-web/src/main/java/org/datagear/web/util/XmlDriverEntityManagerInitializer.java
+++ datagear-web/src/main/java/org/datagear/web/util/XmlDriverEntityManagerInitializer.java
@@ -10,11 +10,14 @@ package org.datagear.web.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.datagear.connection.DriverEntity;
 import org.datagear.connection.XmlDriverEntityManager;
+import org.datagear.util.FileUtil;
 import org.datagear.util.IOUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,30 +70,54 @@ public class XmlDriverEntityManagerInitializer
 		if (LOGGER.isInfoEnabled())
 			LOGGER.info("start init {}", XmlDriverEntityManager.class.getSimpleName());
 
-		// 仅在首次初始化内置驱动，避免升级至使用新驱动的新版本时导致已有数据源功能不可用
-		if (isDriverEntityInfoFileExists())
-		{
-			if (LOGGER.isInfoEnabled())
-				LOGGER.info("init built-in driver is skipped, it will be done only if the directory is empty");
-		}
-		else
-		{
-			InputStream in = null;
+		File driverRootDirectory = this.xmlDriverEntityManager.getRootDirectory();
 
-			try
+		InputStream in = null;
+
+		try
+		{
+			in = getClass().getClassLoader().getResourceAsStream(BUILT_IN_DRIVER_ENTITY_ZIP_CLASS_PATH);
+			ZipInputStream zipIn = IOUtil.getZipInputStream(in);
+
+			// 初次安装，直接解压
+			if (!isDriverEntityInfoFileExists())
 			{
-				in = getClass().getClassLoader().getResourceAsStream(BUILT_IN_DRIVER_ENTITY_ZIP_CLASS_PATH);
-				ZipInputStream zipIn = new ZipInputStream(in);
-
 				IOUtil.unzip(zipIn, this.xmlDriverEntityManager.getRootDirectory());
 			}
-			finally
+			else
 			{
-				IOUtil.close(in);
-			}
+				// 仅更新已有驱动中可能缺失的库文件，而不添加新的驱动库，避免升级至使用新驱动的新版本时导致已有数据源功能不可用
 
-			if (LOGGER.isInfoEnabled())
-				LOGGER.info("init built-in driver is done");
+				ZipEntry zipEntry = null;
+				while ((zipEntry = zipIn.getNextEntry()) != null)
+				{
+					File my = FileUtil.getFile(driverRootDirectory, zipEntry.getName());
+
+					if (!zipEntry.isDirectory() && !my.exists())
+					{
+						File parent = my.getParentFile();
+						if (parent != null && !parent.exists())
+							parent.mkdirs();
+
+						OutputStream out = IOUtil.getOutputStream(my);
+
+						try
+						{
+							IOUtil.write(zipIn, out);
+						}
+						finally
+						{
+							IOUtil.close(out);
+						}
+					}
+
+					zipIn.closeEntry();
+				}
+			}
+		}
+		finally
+		{
+			IOUtil.close(in);
 		}
 
 		this.xmlDriverEntityManager.init();
