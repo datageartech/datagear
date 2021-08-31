30dcecd4b408ac2effd16de51b85f7618ef67679
interestinglife <interestinglife41@163.com>
2021-08-31 20:21:06 +0800

    完善SchemaGuardService.isPermitted()说明

--- datagear-management/src/main/java/org/datagear/management/service/SchemaGuardService.java
+++ datagear-management/src/main/java/org/datagear/management/service/SchemaGuardService.java
@@ -9,6 +9,7 @@ package org.datagear.management.service;
 
 import org.datagear.management.domain.Schema;
 import org.datagear.management.domain.SchemaGuard;
+import org.datagear.util.AsteriskPatternMatcher;
 
 /**
  * {@linkplain SchemaGuard}业务服务接口。
@@ -21,22 +22,8 @@ public interface SchemaGuardService extends EntityService<String, SchemaGuard>
 	/**
 	 * 是否允许创建指定的{@linkplain Schema#getUrl()}。
 	 * <p>
-	 * 实现类应支持{@code *}（表示任意多个字符）匹配规则，例如：
-	 * </p>
-	 * <ul>
-	 * <li>{@code *}<br>
-	 * 匹配任意URL</li>
-	 * <li>{@code *abc}<br>
-	 * 匹配以{@code abc}结尾的URL</li>
-	 * <li>{@code abc*}<br>
-	 * 匹配以{@code abc}开头的URL</li>
-	 * <li>{@code abc*def}<br>
-	 * 匹配以{@code abc}开头、以{@code def}结尾的URL</li>
-	 * <li>{@code *abc*def*}<br>
-	 * 匹配依次包含{@code abc}、{@code def}的URL</li>
-	 * </ul>
-	 * <p>
-	 * 另外，如果没有定义任何{@linkplain SchemaGuard}，应返回{@code true}。
+	 * 实现类应支持{@linkplain AsteriskPatternMatcher}匹配规则，
+	 * 并且，如果没有定义任何{@linkplain SchemaGuard}，应返回{@code true}。
 	 * </p>
 	 * 
 	 * @param schemaURL
