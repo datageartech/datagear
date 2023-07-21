f1b1bcbec3a6bbab6c46d8896e90a023b819e097
zheyangrensheng <zheyangrensheng@163.com>
2023-07-21 14:52:56 +0800

    完善系统对于多种部署数据库的布尔值读写兼容支持

--- datagear-management/src/main/java/org/datagear/management/util/typehandlers/LiteralBooleanTypeHandler.java
+++ datagear-management/src/main/java/org/datagear/management/util/typehandlers/LiteralBooleanTypeHandler.java
@@ -17,11 +17,15 @@
 
 package org.datagear.management.util.typehandlers;
 
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.ibatis.type.BooleanTypeHandler;
 import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.SqlSessionFactoryBean;
 
 /**
  * 使用字面值{@code 'true'}或{@code 'false'}存储{@linkplain Boolean}类型的MyBatis类型映射器。
@@ -32,12 +36,16 @@ import org.apache.ibatis.type.JdbcType;
  * 对于某些需要根据此列值进行判断处理的SQL语句，会出现数据库切换兼容问题。
  * </p>
  * <p>
- * 所以，这里定义此类，将{@linkplain Boolean}类型统一存储为{@code 'true'}和{@code 'false'}（要求数据库列类型为：VARCHAR）。
+ * 所以，这里定义此类，将{@linkplain Boolean}类型统一存储为{@code 'true'}和{@code 'false'}字符串（要求数据库列类型为：VARCHAR）。
+ * </p>
+ * <p>
+ * 注意：此类必须添加下面的{@linkplain MappedTypes}注解，不然使用{@linkplain SqlSessionFactoryBean}执行初始化时无法注册处理{@code boolean}类型。
  * </p>
  * 
  * @author datagear@163.com
  *
  */
+@MappedTypes({ Boolean.class, boolean.class })
 public class LiteralBooleanTypeHandler extends BooleanTypeHandler
 {
 	public LiteralBooleanTypeHandler()
@@ -51,5 +59,44 @@ public class LiteralBooleanTypeHandler extends BooleanTypeHandler
 	{
 		String value = (Boolean.TRUE.equals(parameter) ? "true" : "false");
 		ps.setString(i, value);
+	}
+
+	@Override
+	public Boolean getNullableResult(ResultSet rs, String columnName) throws SQLException
+	{
+		return stringToBoolean(rs.getString(columnName));
+	}
+
+	@Override
+	public Boolean getNullableResult(ResultSet rs, int columnIndex) throws SQLException
+	{
+		return stringToBoolean(rs.getString(columnIndex));
+	}
+
+	@Override
+	public Boolean getNullableResult(CallableStatement cs, int columnIndex) throws SQLException
+	{
+		return stringToBoolean(cs.getString(columnIndex));
+	}
+
+	protected String booleanToString(Boolean b)
+	{
+		return (Boolean.TRUE.equals(b) ? Boolean.TRUE.toString() : Boolean.FALSE.toString());
+	}
+
+	protected boolean stringToBoolean(String s)
+	{
+		if (Boolean.TRUE.toString().equalsIgnoreCase(s))
+		{
+			return true;
+		}
+		else if ("1".equals(s))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
