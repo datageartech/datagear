7ac8756a63ab0c9de93f2f45910e0e1763148dbd
datagear <datagear@163.com>
2023-10-22 17:47:37 +0800

    图表支持库ECharts版本由5.4.2升级至5.4.3

--- datagear-web/src/main/java/org/datagear/web/util/HtmlTplDashboardImportResolver.java
+++ datagear-web/src/main/java/org/datagear/web/util/HtmlTplDashboardImportResolver.java
@@ -105,7 +105,7 @@ public class HtmlTplDashboardImportResolver
 		impts.add(HtmlTplDashboardImport.valueOfJavaScript(BUILTIN_DASHBOARD_IMPORT_NAME_JQUERY,
 				libPrefix + "/jquery-3.6.0/jquery-3.6.0.min.js"));
 		impts.add(HtmlTplDashboardImport.valueOfJavaScript(BUILTIN_DASHBOARD_IMPORT_NAME_ECHARTS,
-				libPrefix + "/echarts-5.4.2/echarts.min.js"));
+				libPrefix + "/echarts-5.4.3/echarts.min.js"));
 		impts.add(HtmlTplDashboardImport.valueOfJavaScript(BUILTIN_DASHBOARD_IMPORT_NAME_ECHARTS_WORDCLOUD,
 				libPrefix + "/echarts-wordcloud-2.0.0/echarts-wordcloud.min.js"));
 		impts.add(HtmlTplDashboardImport.valueOfJavaScript(BUILTIN_DASHBOARD_IMPORT_NAME_ECHARTS_LIQUIDFILL,
