48dcc7459ad670acbb0d2411172702a46343a592
datagear <datagear@163.com>
2025-06-03 22:48:28 +0800

    图表JS对象chart.resultIgnoreFetch()、chart.resultIgnoreFetchOf()函数只有在图表渲染器明确定义了{supportIgnoreFetch:true}附加属性时才起效

--- datagear-web/src/main/resources/org/datagear/web/static/script/chartFactory.js
+++ datagear-web/src/main/resources/org/datagear/web/static/script/chartFactory.js
@@ -5024,6 +5024,7 @@
 	
 	/**
 	 * 获取/设置数据集结果是否是忽略获取的。
+	 * 如果图表渲染器附加属性没有定义{ supportIgnoreFetch: true }，对于获取操作，此函数将如实返回，对于设置操作，将直接抛出异常，不允许调用。
 	 * 
 	 * @param dataSetResult 数据集结果
 	 * @param ignoreFetch 可选，要设置的值，true 忽略；false 不忽略
@@ -5032,18 +5033,24 @@
 	 */
 	chartBase.resultIgnoreFetch = function(dataSetResult, ignoreFetch)
 	{
+		var supportIgnoreFetch = this._supportIgnoreFetch();
+		
 		if(ignoreFetch === undefined)
 		{
 			return (dataSetResult && dataSetResult.ignoreFetch != null ? dataSetResult.ignoreFetch : false);
 		}
 		else
 		{
+			if(!supportIgnoreFetch)
+				throw new Error("feature [ignore-fetch] unsupported");
+			
 			dataSetResult.ignoreFetch = ignoreFetch;
 		}
 	};
 	
 	/**
 	 * 获取/设置指定数据集绑定对应的数据集结果是否是忽略获取的。
+	 * 如果图表渲染器附加属性没有定义{ supportIgnoreFetch: true }，对于获取操作，此函数将如实返回，对于设置操作，将直接抛出异常，不允许调用。
 	 * 
 	 * @param chartResult 图表结果、数据集结果数组
 	 * @param dataSetBind 数据集绑定、索引数值
