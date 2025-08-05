5e6a63d0c92e8517decdf1a30bb3f542b82e09ef
hahajiutian <hahajiutian@163.com>
2025-08-05 21:37:54 +0800

    看板可视编辑模式响应式弹性布局高度样式增加33%、33vh选项

--- datagear-web/src/main/resources/org/datagear/web/static/script/pages/dashboard/include/dashboardDesign.js
+++ datagear-web/src/main/resources/org/datagear/web/static/script/pages/dashboard/include/dashboardDesign.js
@@ -2828,7 +2828,7 @@ $.inflateDashboardDesignEditorForms = function(po)
 				[
 					{name: "auto", value: "auto"},
 					{name: "5%", value: 5}, {name: "10%", value: 10}, {name: "15%", value: 15}, {name: "20%", value: 20}, {name: "25%", value: 25},
-					{name: "30%", value: 30}, {name: "35%", value: 35}, {name: "40%", value: 40}, {name: "45%", value: 45}, {name: "50%", value: 50},
+					{name: "30%", value: 30}, {name: "33.3%", value: 33}, {name: "35%", value: 35}, {name: "40%", value: 40}, {name: "45%", value: 45}, {name: "50%", value: 50},
 					{name: "55%", value: 55}, {name: "60%", value: 60}, {name: "65%", value: 65}, {name: "70%", value: 70}, {name: "75%", value: 75},
 					{name: "80%", value: 80}, {name: "85%", value: 85}, {name: "90%", value: 90}, {name: "95%", value: 95}, {name: "100%", value: 100}
 				],
@@ -2846,7 +2846,7 @@ $.inflateDashboardDesignEditorForms = function(po)
 				[
 					{name: "auto", value: "auto"},
 					{name: "5vh", value: 5}, {name: "10vh", value: 10}, {name: "15vh", value: 15}, {name: "20vh", value: 20}, {name: "25vh", value: 25},
-					{name: "30vh", value: 30}, {name: "35vh", value: 35}, {name: "40vh", value: 40}, {name: "45vh", value: 45}, {name: "50vh", value: 50},
+					{name: "30vh", value: 30}, {name: "33.3vh", value: 33}, {name: "35vh", value: 35}, {name: "40vh", value: 40}, {name: "45vh", value: 45}, {name: "50vh", value: 50},
 					{name: "55vh", value: 55}, {name: "60vh", value: 60}, {name: "65vh", value: 65}, {name: "70vh", value: 70}, {name: "75vh", value: 75},
 					{name: "80vh", value: 80}, {name: "85vh", value: 85}, {name: "90vh", value: 90}, {name: "95vh", value: 95}, {name: "100vh", value: 100}
 				]
